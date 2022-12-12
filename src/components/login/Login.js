3ba21f94dcdb6586af3b0a59ae7a18acbab17aea
Aliyan27 <aliyanmuhammad425@gmail.com>
2022-12-12 19:08:05 +0500

    Add existing file

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,49 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import "./login.css";
 
 const Login = () => {
+  const validate = Yup.object({
+    email: Yup.string().email().required(),
+    password: Yup.string().min(6).required(),
+  });
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: { validate },
+
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+      console.log(values);
+    },
+  });
   return (
     <>
-      <div className="Container">login</div>
+      <div className="Container login">
+        <h2>My Account</h2>
+        <form onSubmit={formik.handleSubmit}>
+          <label>Email: </label>
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          <label>Password: </label>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+          <button type="submit">LOGIN</button>
+        </form>
+      </div>
     </>
   );
 };
